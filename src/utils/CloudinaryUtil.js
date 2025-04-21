0605830d2382ddc6f507cc9ef7b939b82e95c092
Harshil-231 <harshilpanchal1523@gmail.com>
2025-04-21 20:50:50 +0530

    cloudinary conf removed

--- src/utils/CloudinaryUtil.js
+++ src/utils/CloudinaryUtil.js
@@ -3,12 +3,7 @@ const cloudinary = require("cloudinary").v2;
 const uploadFileToCloudinary = async (file) => {
     try {
         // Configuration
-        cloudinary.config({
-            cloud_name: "dhxgxqwf6",
-            api_key: "832717434777328",
-            api_secret: "dN4sIMYKfO8SYWSFWt8n0qgrl1E"
-        });
-
+       
         // Upload the image
         const cloudinaryResponse = await cloudinary.uploader.upload(file.path, {
             folder: "owner_profiles"  // Optional:  Organize your images in Cloudinary
